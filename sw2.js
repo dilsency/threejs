407144c6633b07e6c134ec10c1f07c88fd05ef3f
dilsency <dilsency@gmail.com>
2025-11-13 09:18:21 +0100

    changed paths to relative, doesn't work for local but let's see

--- sw2.js
+++ sw2.js
@@ -1,36 +1,35 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('threejs').then((cache) => cache.addAll([
-      '/threejs/',
-      '/threejs/index.html',
-      '/threejs/main.js',
+      '../threejs/',
+      '../threejs/index.html',
+      '../threejs/main.js',
 
-      '/threejs/models/Dodecahedron.obj',
-      '/threejs/models/Icosahedron.gltf',
-      '/threejs/models/Icosahedron.obj',
+      '../threejs/models/Dodecahedron.obj',
+      '../threejs/models/Icosahedron.gltf',
+      '../threejs/models/Icosahedron.obj',
 
-      '/threejs/textures/bunny_thickness_2.jpg',
-      '/threejs/textures/bunny_thickness.jpg',
-      '/threejs/textures/texture_default.jpg',
-      '/threejs/textures/white.jpg',
+      '../threejs/textures/bunny_thickness_2.jpg',
+      '../threejs/textures/bunny_thickness.jpg',
+      '../threejs/textures/texture_default.jpg',
+      '../threejs/textures/white.jpg',
 
-      '/threejs/textures/cube/skybox/nx.png',
-      '/threejs/textures/cube/skybox/ny.png',
-      '/threejs/textures/cube/skybox/nz.png',
-      '/threejs/textures/cube/skybox/px.png',
-      '/threejs/textures/cube/skybox/py.png',
-      '/threejs/textures/cube/skybox/pz.png',
+      '../threejs/textures/cube/skybox/nx.png',
+      '../threejs/textures/cube/skybox/ny.png',
+      '../threejs/textures/cube/skybox/nz.png',
+      '../threejs/textures/cube/skybox/px.png',
+      '../threejs/textures/cube/skybox/py.png',
+      '../threejs/textures/cube/skybox/pz.png',
 
-      '/threejs/shaders/fragment1.js',
-      '/threejs/shaders/vertex1.js',
+      '../threejs/shaders/fragment1.js',
+      '../threejs/shaders/vertex1.js',
 
-        '/threejs/helpers/helper_camera_rotation.js',
-        '/threejs/helpers/helper_generation_hud.js',
-        '/threejs/helpers/helper_generation_planet.js',
-        '/threejs/helpers/helper_mesh.js',
+        '../threejs/helpers/helper_camera_rotation.js',
+        '../threejs/helpers/helper_generation_hud.js',
+        '../threejs/helpers/helper_mesh.js',
 
-        '/threejs/img/icon 192x192.png',
-        '/threejs/img/icon 512x512.png',
+        '../threejs/img/icon 192x192.png',
+        '../threejs/img/icon 512x512.png',
 
     ])),
   );
